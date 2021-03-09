241ed02364915d10ecc5cab6c2d312044cf84b8b
tvu2000 <ttvu39400@gmail.com>
2021-03-09 13:19:22 -0800

    update app.js with express and ejs, and organize files

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express"); 
 const bodyPar = require("body-parser"); 
-app.use(bodyPar.urlencoded({extended: true}));
 const app = express(); 
+const ejs = require("ejs"); 
+app.set('view engine', 'ejs');
+app.use(express.static("public")); 
+
+app.use(bodyPar.urlencoded({extended: true}));
 
 app.listen(300, function(){
-    console.log("Local host 3000")
+    console.log("Local host 300");
 })
 
 app.get("/", function(req, res){
-    
+    res.render("home-page.ejs"); 
+})
+
+app.get("/tweet", function(req, res){
+    res.render("tweet.ejs"); 
+})
+
+app.get("/US", function(req, res){
+    res.render("tweet.ejs"); 
+})
+
+app.get("/vaccine", function(req, res){
+    res.render("tweet.ejs"); 
 })
